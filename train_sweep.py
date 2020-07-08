42e29327519f090e6d7e05bb7124a266ed50eb10
Alex <52916777+TheBigTicket02@users.noreply.github.com>
2020-07-08 14:15:01 +0300

    working Eff

--- train_sweep.py
+++ train_sweep.py
@@ -12,8 +12,8 @@ from pytorch_lightning.metrics.functional.classification import accuracy
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, LearningRateLogger
 from pytorch_lightning.loggers import WandbLogger
 from torch import optim
-from torch.optim.lr_scheduler import MultiStepLR
-#from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import OneCycleLR
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torchvision import models, transforms
 from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import ImageFolder
@@ -21,184 +21,48 @@ from torchvision.datasets import ImageFolder
 import wandb
 
 from collections import OrderedDict
-from typing import Optional, Generator, Union
-from torch.optim.optimizer import Optimizer
-from torch.nn import Module
-
-BN_TYPES = (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)
-
-#  --- Utility functions ---
-
-
-def _make_trainable(module: Module) -> None:
-    """Unfreezes a given module.
-    Args:
-        module: The module to unfreeze
-    """
-    for param in module.parameters():
-        param.requires_grad = True
-    module.train()
-
-
-def _recursive_freeze(module: Module,
-                      train_bn: bool = True) -> None:
-    """Freezes the layers of a given module.
-    Args:
-        module: The module to freeze
-        train_bn: If True, leave the BatchNorm layers in training mode
-    """
-    children = list(module.children())
-    if not children:
-        if not (isinstance(module, BN_TYPES) and train_bn):
-            for param in module.parameters():
-                param.requires_grad = False
-            module.eval()
-        else:
-            # Make the BN layers trainable
-            _make_trainable(module)
-    else:
-        for child in children:
-            _recursive_freeze(module=child, train_bn=train_bn)
-
-
-def freeze(module: Module,
-           n: Optional[int] = None,
-           train_bn: bool = True) -> None:
-    """Freezes the layers up to index n (if n is not None).
-    Args:
-        module: The module to freeze (at least partially)
-        n: Max depth at which we stop freezing the layers. If None, all
-            the layers of the given module will be frozen.
-        train_bn: If True, leave the BatchNorm layers in training mode
-    """
-    children = list(module.children())
-    n_max = len(children) if n is None else int(n)
-
-    for child in children[:n_max]:
-        _recursive_freeze(module=child, train_bn=train_bn)
-
-    for child in children[n_max:]:
-        _make_trainable(module=child)
-
-
-def filter_params(module: Module,
-                  train_bn: bool = True) -> Generator:
-    """Yields the trainable parameters of a given module.
-    Args:
-        module: A given module
-        train_bn: If True, leave the BatchNorm layers in training mode
-    Returns:
-        Generator
-    """
-    children = list(module.children())
-    if not children:
-        if not (isinstance(module, BN_TYPES) and train_bn):
-            for param in module.parameters():
-                if param.requires_grad:
-                    yield param
-    else:
-        for child in children:
-            for param in filter_params(module=child, train_bn=train_bn):
-                yield param
-
-
-def _unfreeze_and_add_param_group(module: Module,
-                                  optimizer: Optimizer,
-                                  lr: Optional[float] = None,
-                                  train_bn: bool = True):
-    """Unfreezes a module and adds its parameters to an optimizer."""
-    _make_trainable(module)
-    params_lr = optimizer.param_groups[0]['lr'] if lr is None else float(lr)
-    optimizer.add_param_group(
-        {'params': filter_params(module=module, train_bn=train_bn),
-         'lr': params_lr / 10.,
-         })
-
+from typing import Optional
+from efficientnet_pytorch import EfficientNet
 
 wandb.login(key='44b74d6614becfad4329893ea0144da65336bdbd')
 
-# Sweep parameters
-hyperparameter_defaults = dict(
-    backbone='resnet50',
-    train_bn = True,
-    gpus=1,
-    batch_size = 60,
-    lr = 3e-3,
-    #lr_scheduler_gamma = 1e-1,
-    epochs = 10,
-    num_workers = 4
-    #milestones = (5,10)
-)
-
-wandb.init(config=hyperparameter_defaults)
-config = wandb.config
-
-class Cars(LightningModule):
+class EffNet(LightningModule):
 
     def __init__(self, 
-                hparams):
+                train_bn: bool = True,
+                batch_size: int = 16,
+                lr: float = 5e-4,
+                num_workers: int = 4,
+                factor: float = 0.5,
+                pct_start: Optional[float] = None,
+                anneal_strategy: Optional[str] = None,
+                **kwargs):
         super().__init__()
-        self.backbone = hparams.backbone
-        self.train_bn = hparams.train_bn
-        self.batch_size = hparams.batch_size
-        self.lr = hparams.lr
-        #self.lr_scheduler_gamma = hparams.lr_scheduler_gamma
-        self.num_workers = hparams.num_workers
-        #self.milestones = hparams.milestones
+        self.train_bn = train_bn
+        self.batch_size = batch_size
+        self.lr = lr
+        self.num_workers = num_workers
+        self.factor = factor
+        self.pct_start = pct_start
+        self.anneal_strategy = anneal_strategy
+        self.save_hyperparameters()
 
         self.__build_model()
         
     def __build_model(self):
         num_target_classes = 196
-        model_func = getattr(models, self.backbone)
-        backbone = model_func(pretrained=True)
-    
-        _layers = list(backbone.children())[:-1]
-        self.feature_extractor = nn.Sequential(*_layers)
-        freeze(module=self.feature_extractor, train_bn=self.train_bn)
+        self.net = EfficientNet.from_pretrained('efficientnet-b5')
+        in_features = self.net._fc.in_features
 
-        _fc_layers = [nn.Linear(2048, 1024),
-                     nn.Linear(1024, 512),
-                     nn.Linear(512, num_target_classes)]
-        self.fc = nn.Sequential(*_fc_layers)
+        _fc_layers = [nn.Linear(in_features, num_target_classes)] #add hyper
+                     #nn.Linear(1024, 512), #hyper
+                     #nn.Linear(512, num_target_classes)]
+        self.net._fc = nn.Sequential(*_fc_layers)
 
     def forward(self, x):
 
-        x = self.feature_extractor(x)
-        x = x.squeeze(-1).squeeze(-1)
-
-        x = self.fc(x)
-
-        return x
+        return self.net.forward(x)
     
-    #def train(self, mode=True):
-    #    super().train(mode=mode)
-
-    #    epoch = self.current_epoch
-    #    if epoch < self.milestones[0] and mode:
-    #        # feature extractor is frozen (except for BatchNorm layers)
-    #        freeze(module=self.feature_extractor,
-    #               train_bn=self.train_bn)
-
-    #    elif self.milestones[0] <= epoch < self.milestones[1] and mode:
-            # Unfreeze last two layers of the feature extractor
-    #        freeze(module=self.feature_extractor,
-    #               n=-2,
-    #               train_bn=self.train_bn)
-
-    #def on_epoch_start(self):
-    #    """Use `on_epoch_start` to unfreeze layers progressively."""
-    #    optimizer = self.trainer.optimizers[0]
-    #    if self.current_epoch == self.milestones[0]:
-    #        _unfreeze_and_add_param_group(module=self.feature_extractor[-2:],
-    #                                      optimizer=optimizer,
-    #                                      train_bn=self.train_bn)
-
-    #    elif self.current_epoch == self.milestones[1]:
-    #        _unfreeze_and_add_param_group(module=self.feature_extractor[:-2],
-    #                                      optimizer=optimizer,
-    #                                      train_bn=self.train_bn)
-
     def training_step(self, batch, batch_idx):
 
         # 1. Forward pass:
@@ -214,7 +78,7 @@ class Cars(LightningModule):
         output = OrderedDict({'loss': train_loss,
                                'train_acc': acc,
                               'log': tqdm_dict,
-                              'progress_bar': tqdm_dict})
+                             'progress_bar': tqdm_dict})
 
         return output
 
@@ -227,9 +91,6 @@ class Cars(LightningModule):
         
         tensorboard_logs = {'train_loss': train_loss_mean, 'train_acc': avg_acc}
         return {'train_loss': train_loss_mean,  'log': tensorboard_logs}
-        #return {'log': {'train_loss': train_loss_mean,
-        #                'train_acc': train_acc_mean,
-        #                'step': self.current_epoch}}
 
     def validation_step(self, batch, batch_idx):
 
@@ -239,10 +100,9 @@ class Cars(LightningModule):
 
         # 2. Compute loss & accuracy:
         val_loss = F.cross_entropy(y_logits, y)
-        acc = accuracy(y_logits, y)#y_true)
+        acc = accuracy(y_logits, y)
 
-        return {'val_loss': val_loss, 'val_acc': acc
-               }#'num_correct': num_correct}
+        return {'val_loss': val_loss, 'val_acc': acc}
 
     def validation_epoch_end(self, outputs):
         """Compute and log validation loss and accuracy at the epoch level."""
@@ -253,21 +113,30 @@ class Cars(LightningModule):
         
         tensorboard_logs = {'val_loss': val_loss_mean, 'val_acc': avg_acc}
         return {'val_loss': val_loss_mean,  'log': tensorboard_logs}
-        #return {'log': {'val_loss': val_loss_mean,
-        #                'val_acc': val_acc_mean,
-        #                'step': self.current_epoch}}
         
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(filter(lambda p: p.requires_grad,
+        if (self.pct_start == None) and (self.anneal_strategy == None): #fix or
+        
+            optimizer = optim.Adam(filter(lambda p: p.requires_grad,
                                       self.parameters()),
-                               lr=self.lr)
+                                   lr=self.lr)
+            lr_scheduler = {'scheduler': ReduceLROnPlateau(optimizer, factor=self.factor, patience=2),'name': 'learning_rate'}
+            return [optimizer], [lr_scheduler]
+        
+        else:
+            
+            optimizer = optim.Adam(filter(lambda p: p.requires_grad,
+                                      self.parameters()),
+                                   lr=self.lr)
+            
+            scheduler = OneCycleLR(optimizer,
+                            max_lr=self.lr,
+                            #steps = self.steps,
+                            epochs=15, steps_per_epoch=1,
+                            pct_start=self.pct_start, anneal_strategy=self.anneal_strategy)
 
-        #scheduler = MultiStepLR(optimizer,
-        #                        milestones=self.milestones,
-        #                        gamma=self.lr_scheduler_gamma)
-
-        return optimizer#], [scheduler]
+        return [optimizer], [scheduler]
 
     def setup(self, stage: str):
         data_dir = '../input/stanford-car-dataset-by-classes-folder/car_data/car_data'
@@ -311,12 +180,23 @@ class Cars(LightningModule):
                             pin_memory=True)
 
 
-def main(hparams, default: Optional[dict] = hyperparameter_defaults):
+hyper = dict(
+    train_bn = True,
+    gpus=1,
+    batch_size = 16,
+    lr = 5e-4,
+    factor = 0.5,
+    num_workers = 4
+    #pct_start = 0.3,
+    #anneal_strategy = 'cos'
+)
+
+def main(default=hyper):
     # ------------------------
     # 1 INIT LIGHTNING MODEL
     # ------------------------
     seed_everything(42)
-    model = Cars(hparams)
+    model = EffNet()
 
     # ------------------------
     # 2 SET WANDB LOGGER
@@ -324,23 +204,25 @@ def main(hparams, default: Optional[dict] = hyperparameter_defaults):
 
     # Sweep parameters
     
-    wandb_logger = WandbLogger(name='Test8', project="Cars")
+    wandb_logger = WandbLogger(name='Eff1', project="Cars")
     
     wandb_logger.log_hyperparams(default)
 
     checkpoint_cb = ModelCheckpoint(filepath = './cars-{epoch:02d}-{val_acc:.4f}',monitor='val_acc', mode='max')
+    early = EarlyStopping(patience=3, monitor='val_acc', mode='max')
     # ------------------------
     # 3 INIT TRAINER
     # ------------------------
     trainer = Trainer(
-        gpus=hparams.gpus,
+        gpus=1,
         logger=wandb_logger,
-        max_epochs=hparams.epochs,
+        max_epochs=15,
         progress_bar_refresh_rate=10,
         deterministic=True,
         precision=16,
-        checkpoint_callback=checkpoint_cb
-        #callbacks=[LearningRateLogger()],
+        checkpoint_callback=checkpoint_cb,
+        early_stop_callback=early,
+        callbacks=[LearningRateLogger()],
     )
 
     # ------------------------
@@ -351,4 +233,4 @@ def main(hparams, default: Optional[dict] = hyperparameter_defaults):
     wandb.save(checkpoint_cb.best_model_path)
 
 if __name__ == '__main__':
-    main(config)
+    main()
